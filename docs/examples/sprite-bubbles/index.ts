b7ac8fdf0d426222cd3308076e59f2a53b8e413c
YunYouJun <me@yunyoujun.cn>
2024-10-06 06:47:01 +0800

    chore: fix lint

--- docs/examples/sprite-bubbles/index.ts
+++ docs/examples/sprite-bubbles/index.ts
@@ -1,129 +1,156 @@
-// import type { Application } from 'pixi.js'
-// import { Assets, Sprite } from 'pixi.js'
+import type { Application } from 'pixi.js'
+import { Assets, Container, Graphics, Point, Sprite } from 'pixi.js'
 
-// async function preload() {
-//   const assets = [
-//     { alias: 'background', src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg' },
-//   ]
-//   await Assets.load(assets)
-// }
+async function preload() {
+  const assets = [
+    { alias: 'background', src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg' },
+  ]
+  await Assets.load(assets)
+}
 
-// function addEvents() {
-//   document.addEventListener('mousedown', (e) => {
-//     mousedown = true
-//   })
-//   document.addEventListener('mouseup', (e) => {
-//     mousedown = false
-//   })
-//   document.addEventListener('mousemove', (e) => {
-//     if (!mousedown)
-//       return false
-//     const bubble = new Bubble({
-//       x: e.clientX,
-//       y: e.clientY,
-//     })
-//     container.addChild(bubble.sprite)
-//     bubbles.push(bubble)
-//   }, false)
-// }
+const bubbles: Bubble[] = []
+let mousedown = false
 
-// function displacement() {
-//   displacementSprite = PIXI.Sprite.from('https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png')
-//   displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
-//   displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite)
-//   displacementFilter.padding = 0
+let displacementSprite: Sprite
 
-//   displacementSprite.position = container.position
+function displacement() {
+  // displacementSprite = Sprite.from('https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png')
+  // displacementSprite.texture.baseTexture.wrapMode = WRAP_MODES.REPEAT
+  // displacementFilter = new filters.DisplacementFilter(displacementSprite)
+  // displacementFilter.padding = 0
 
-//   app.stage.addChild(displacementSprite)
+  // displacementSprite.position = container.position
 
-//   container.filters = [displacementFilter]
+  // app.stage.addChild(displacementSprite)
 
-//   displacementFilter.scale.x = 50
-//   displacementFilter.scale.y = 50
-// }
+  // container.filters = [displacementFilter]
 
-// function animate() {
-//   requestAnimationFrame(animate)
-//   render()
-// }
+  // displacementFilter.scale.x = 50
+  // displacementFilter.scale.y = 50
+}
 
-// function removeBubble(_bubble) {
-//   const index = bubbles.indexOf(_bubble)
-//   bubbles.splice(index, 1)
-//   container.removeChildren(index, index + 1)
-// }
+class Bubble {
+  sprite: Sprite
+  position: Point
+  velocity: Point
+  acceleration: Point
+  pressure: Point
+  waterResistance: Point
+  scale: number
+  mass: number
 
-// function render() {
-//   for (let i = 0; i < bubbles.length; i++) {
-//     const bubble = bubbles[i]
-//     if (bubble.isDead())
-//       removeBubble(bubble)
-//     else bubble.draw(graphics)
-//   }
-//   displacementSprite.x++
-//   if (displacementSprite.x > displacementSprite.width)
-//     displacementSprite.x = 0
-// }
+  constructor(args) {
+    if (args === undefined)
+      args = {}
+    this.sprite = Sprite.from('https://res.cloudinary.com/losrodriguez/image/upload/v1566328006/black_bubble_g9jolh.png')
+    this.position = new Point(args.x, args.y)
+    this.init()
+  }
 
-// var Bubble = function (args) {
-//   if (args === undefined)
-//     args = {}
-//   this.sprite = PIXI.Sprite.from('https://res.cloudinary.com/losrodriguez/image/upload/v1566328006/black_bubble_g9jolh.png')
-//   this.position = new Vector(args.x, args.y)
-//   this.init = function () {
-//     this.velocity = new Vector(Math.random() * 0.1 - 0.05, -0.01)
-//     this.acceleration = new Vector(Math.random() * 0.01 - 0.05, -0.01)
-//     this.pressure = new Vector(Math.random() * 0.1 - 0.05, -0.1)
-//     this.waterResistance = new Vector(Math.random() * 0.1 - 0.05, -0.1)
-//     this.scale = Math.random() * 0.5
-//     this.mass = Math.random() * 0.1
-//     this.sprite.rotation = Math.random() * Math.PI
-//     this.draw()
-//   }
-//   this.draw = function (_graphics) {
-//     this.update()
-//     this.sprite.x = this.position.x
-//     this.sprite.y = this.position.y
-//     this.sprite.scale.set(this.scale)
-//   }
-//   this.applyForce = function (_force) {
-//     const f = _force.mult(this.mass)
-//     this.acceleration.addTo(f)
-//   }
-//   this.update = function () {
-//     this.applyForce(this.pressure)
-//     this.applyForce(this.waterResistance)
-//     this.velocity.addTo(this.acceleration)
-//     this.position.addTo(this.velocity)
-//     this.acceleration.mult(0)
-//     this.scale -= 0.005
-//   }
-//   this.isDead = function () {
-//     return (this.scale < 0.0)
-//   }
-//   this.init()
-//   return this
-// }
+  init() {
+    this.velocity = new Point(Math.random() * 0.1 - 0.05, -0.01)
+    this.acceleration = new Point(Math.random() * 0.01 - 0.05, -0.01)
+    this.pressure = new Point(Math.random() * 0.1 - 0.05, -0.1)
+    this.waterResistance = new Point(Math.random() * 0.1 - 0.05, -0.1)
+    this.scale = Math.random() * 0.5
+    this.mass = Math.random() * 0.1
+    this.sprite.rotation = Math.random() * Math.PI
+    this.draw()
+  }
 
-// export async function createExample(app: Application) {
-//   await preload()
+  draw(_graphics?: Graphics) {
+    this.update()
+    this.sprite.x = this.position.x
+    this.sprite.y = this.position.y
+    this.sprite.scale.set(this.scale)
+  }
 
-//   const background = Sprite.from('background')
-//   background.anchor.set(0.5)
-//   background.scale.set(2)
-//   app.stage.addChild(background)
+  applyForce(_force) {
+    const f = _force.mult(this.mass)
+    this.acceleration.add(f)
+  }
 
-//   for (let i = 0; i < 300; i++) {
-//     const bubble = new Bubble({
-//       x: Math.random() * app.screen.width,
-//       y: Math.random() * app.screen.height,
-//     })
-//     container.addChild(bubble.sprite)
-//     bubbles.push(bubble)
-//   }
-//   displacement()
-//   addEvents()
-// }
+  update() {
+    this.applyForce(this.pressure)
+    this.applyForce(this.waterResistance)
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
+    this.acceleration.multiply(new Point(0, 0))
+    this.scale -= 0.005
+  }
 
-// export default createExample
+  isDead() {
+    return (this.scale < 0.0)
+  }
+}
+
+export async function createExample(app: Application) {
+  await preload()
+
+  const background = Sprite.from('background')
+  background.anchor.set(0.5)
+  background.scale.set(2)
+  app.stage.addChild(background)
+
+  const container = new Container()
+  const graphics = new Graphics()
+
+  function addEvents() {
+    document.addEventListener('mousedown', (_e) => {
+      mousedown = true
+    })
+    document.addEventListener('mouseup', (_e) => {
+      mousedown = false
+    })
+    document.addEventListener('mousemove', (e) => {
+      if (!mousedown)
+        return false
+      const bubble = new Bubble({
+        x: e.clientX,
+        y: e.clientY,
+      })
+      container.addChild(bubble.sprite)
+      bubbles.push(bubble)
+    }, false)
+  }
+
+  function removeBubble(_bubble) {
+    const index = bubbles.indexOf(_bubble)
+    bubbles.splice(index, 1)
+    container.removeChildren(index, index + 1)
+  }
+
+  function render() {
+    for (let i = 0; i < bubbles.length; i++) {
+      const bubble = bubbles[i]
+      if (bubble.isDead())
+        removeBubble(bubble)
+      else bubble.draw(graphics)
+    }
+    displacementSprite.x++
+    if (displacementSprite.x > displacementSprite.width)
+      displacementSprite.x = 0
+  }
+
+  function animate() {
+    requestAnimationFrame(animate)
+    render()
+  }
+
+  for (let i = 0; i < 300; i++) {
+    const bubble = new Bubble({
+      x: Math.random() * app.screen.width,
+      y: Math.random() * app.screen.height,
+    })
+    container.addChild(bubble.sprite)
+    bubbles.push(bubble)
+  }
+  displacement()
+  addEvents()
+
+  return {
+    animate,
+  }
+}
+
+export default createExample

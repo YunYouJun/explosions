bd99cfe9694f72f8700b352924b183b279bad037
YunYouJun <me@yunyoujun.cn>
2025-04-16 22:28:28 +0800

    feat(explosions): build deps & only esm & animejs@4

--- packages/fireworks/build.config.ts
+++ packages/fireworks/build.config.ts
@@ -6,11 +6,4 @@ export default defineBuildConfig({
   ],
   declaration: true,
   clean: true,
-  rollup: {
-    emitCJS: true,
-  },
-  externals: [
-    'animejs',
-    '@ctrl/tinycolor',
-  ],
 })
